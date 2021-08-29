a5a5bfa522c25a3d14f44db8956faec3dde36052
dennis-runkowski <dennis.runkowski28@gmail.com>
2021-08-29 19:34:07 -0400

    test cases for data loaders

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This package is to process data before using in a machine learning model or for 
 
 As we all know the hardest part in any fancy machine learning problem is the data cleaning and wrangling. This library helps clean, normalize and tokenize you data in a simple json based pipeline.
 
-See the documentation to get started. https://denrun-p.github.io/data_preprocessing/html/data_preprocessing.html
+See the documentation to get started. https://dennis-runkowski.github.io/data_preprocessing/html/data_preprocessing.html
 
 
 ## Documentation
--- test_cases/test_data_loaders.py
+++ test_cases/test_data_loaders.py
@@ -1,17 +1,16 @@
-from time import process_time
 import unittest
 
+from data_preprocessing import DataPreprocess
 from data_preprocessing.steps.data_loaders.single_item import SingleItemLoader
 from data_preprocessing.steps.data_loaders.list_loader import ListDataLoader
 from data_preprocessing.steps.data_loaders.csv_loader import CsvDataLoader
 
 
 TEST_DATA = "This is a TEST sentence!"
-TEST_LIST = ['this is a test', 'I like dogs', "The jets suck."]
+TEST_LIST = ["this is a test", "I like dogs", "The jets suck."]
 
 
 class TestDataLoaders(unittest.TestCase):
-
     def test_single_item(self):
         loader = SingleItemLoader()
         process = loader.process(TEST_DATA)
@@ -21,7 +20,7 @@ class TestDataLoaders(unittest.TestCase):
         loader = ListDataLoader()
         data = []
         for batch in loader.process(TEST_LIST):
-            data.append(batch['data'])
+            data.append(batch["data"])
         self.assertEqual(TEST_LIST, data)
 
     def test_csv_loader(self):
@@ -30,18 +29,79 @@ class TestDataLoaders(unittest.TestCase):
             "type": "csv",
             "file_path": "test_data/test.csv",
             "columns": {
-                "id": "id",
-                "data": "text",
-                "additional_columns": ["username"]
+                "id": "id", "data": "text", "additional_columns": ["username"]
             },
             "batch_size": 1000,
             "log_level": "INFO",
-            "preserve_original": False
+            "preserve_original": False,
         }
         loader = CsvDataLoader(config)
-        data = [item['data'] for item in loader.process()]
+        data = [item["data"] for item in loader.process()]
         self.assertEqual(4, len(data))
+
+    def test_single_item_pipeline(self):
+        config = {
+            "data_loader": {"type": "single_item"},
+            "steps": [
+                {
+                    "name": "normalize_text",
+                    "type": "lowercase",
+                    "log_level": "INFO"
+                },
+            ],
+        }
+        loader = DataPreprocess(config)
+        process = loader.process_item(TEST_DATA)
+        self.assertEqual(TEST_DATA.lower(), process["data"])
+
+    def test_list_pipeline(self):
+        config = {
+            "data_loader": {"type": "list"},
+            "steps": [
+                {
+                    "name": "normalize_text",
+                    "type": "lowercase",
+                    "log_level": "INFO"
+                },
+            ],
+        }
+        loader = DataPreprocess(config)
+        data = []
+        for batch in loader.process_data(TEST_LIST):
+            for item in batch:
+                data.append(item["data"])
+
+        test = [item.lower() for item in TEST_LIST]
+        self.assertEqual(test, data)
+
+    def test_csv_pipeline(self):
+        config = {
+            "data_loader": {
+                "name": "data_loader",
+                "type": "csv",
+                "file_path": "test_data/test.csv",
+                "columns": {
+                    "id": "id", "data": "text", 
+                    "additional_columns": ["username"]
+                },
+            },
+            "steps": [
+                {
+                    "name": "normalize_text",
+                    "type": "lowercase",
+                    "log_level": "INFO"
+                },
+            ],
+        }
+        loader = DataPreprocess(config)
+        data = []
+        for batch in loader.process_data():
+            for item in batch:
+                data.append(item["data"])
+
+        self.assertEqual(4, len(data))
+
 
 if __name__ == "__main__":
     unittest.main()
-    # pass
+
